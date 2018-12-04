6da373313348df4902b355907e80a949e3191829
srinivashyd77 <srinivashyd77@gmail.com>
2018-12-05 00:32:55 +0530

    added soap client template

---
+++ src/main/java/com/dell/fil/soapclient/Config.java
@@ -0,0 +1,25 @@
+package com.dell.fil.soapclient;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+
+@Configuration
+public class Config {
+	@Bean
+	public Jaxb2Marshaller marshaller() {
+		Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
+		marshaller.setContextPath("com.dell.fil.client");
+		return marshaller;
+	}
+
+	@Bean
+	public SOAPConnector soapConnector(Jaxb2Marshaller marshaller) {
+		SOAPConnector client = new SOAPConnector();
+		//client.setDefaultUri("http://localhost:8080/service/student-details");
+		client.setMarshaller(marshaller);
+		client.setUnmarshaller(marshaller);
+		return client;
+	}
+
+}
